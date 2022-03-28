73c328f703e575614c6f30613d244870d5526ccc
ibnlisha <ibn.lisha@yahoo.com>
2022-03-28 11:06:08 +0300

    remove authentication

--- src/components/Admin/AdminPage.jsx
+++ src/components/Admin/AdminPage.jsx
@@ -63,7 +63,10 @@ const AdminPage = () => {
                 <li className='profile'><FontAwesomeIcon icon = {faUser} /></li>
                 {isAuthenticated &&
                 <li><button 
-                onClick={()=>dispatch(addCurrentUser({userInfo: {}}))}
+                onClick={()=>{
+                  dispatch(addCurrentUser({userInfo: {}}))
+                  setIsAuthenticated(false)
+                }}
                 className='btn outline'>Logout</button></li>
               }
             </nav>
